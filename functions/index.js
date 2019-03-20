ab69b4d75d7ddf412ef5d46099d2172fe4470af1
iwilliamlee <willster4321@gmail.com>
2019-03-20 12:01:36 -0700

    + url endpoint getUpdatedShows. Will take all sample data and update the epoch time. won't return anything

--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,7 @@ const CHANNEL_2_COLLECTION = 'channel_2';
 const CHANNEL_3_COLLECTION = 'channel_3';
 const USER_COLLECTION = 'user';
 const USER_ONLY_ID = 'only_user';
+const fs = require('fs');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -36,16 +37,49 @@ appPostShow.post('', (req, res) => {
 exports.postShow = functions.https.onRequest(appPostShow);
 
 
-//Updates the entire database with new data
+//Updates the entire database with up to date data
 const appUpdateShow = express();
 appUpdateShow.get('', (req, res) => {
 
     var channel_1_Ref = db.collection(CHANNEL_1_COLLECTION);
     var channel_2_Ref = db.collection(CHANNEL_2_COLLECTION);
     var channel_3_Ref = db.collection(CHANNEL_3_COLLECTION);
+    var userRef = db.collection(USER_COLLECTION).doc(USER_ONLY_ID);
+
+    var currentMillis = (new Date).getTime();
+
+    var channel_1_json = JSON.parse(fs.readFileSync('./database/Channel_1.json'));
+    var channel_2_json = JSON.parse(fs.readFileSync('./database/Channel_2.json'));
+    var channel_3_json = JSON.parse(fs.readFileSync('./database/Channel_3.json'));
     
-    var milliseconds = (new Date).getTime();
-    res.status(200).send('OK' + milliseconds);
+    var batch = db.batch();
+
+    //Remove user's current recording too
+    batch.update(userRef, {currentRecording: null});
+
+    var milliSecondOffSet = Math.floor(currentMillis/1000);
+
+    for (var i = 0; i < channel_1_json.length; i++) {
+        channel_1_json[i].startTime = milliSecondOffSet;
+        milliSecondOffSet += (channel_1_json[i].duration);
+        batch.set(channel_1_Ref.doc('' + channel_1_json[i].id), channel_1_json[i]);
+    }
+    for (var i = 0; i < channel_2_json.length; i++) {
+        channel_2_json[i].startTime = milliSecondOffSet;
+        milliSecondOffSet += (channel_2_json[i].duration);
+        batch.set(channel_2_Ref.doc('' + channel_2_json[i].id), channel_2_json[i]);
+    }
+    for (var i = 0; i < channel_3_json.length; i++) {
+        channel_3_json[i].startTime = milliSecondOffSet;
+        milliSecondOffSet += (channel_3_json[i].duration );
+        batch.set(channel_3_Ref.doc('' + channel_3_json[i].id), channel_3_json[i]);
+    }
+
+    return batch.commit().then(result => {
+        return res.send('OK');
+    }).catch(error => {
+        return res.status(500).send(error);
+    });
 });
 exports.getUpdatedShows = functions.https.onRequest(appUpdateShow);
 
@@ -53,7 +87,9 @@ const appGetChannels = express();
 appGetChannels.get('', (req, res) => {
     var channelNumber = req.query.channel_number;
     var channelCol = 'channel_' + channelNumber;
+
     
+
     var first = db.collection(channelCol).limit(10);
     return first.get().then((snapshot) => {
         var result = [];
